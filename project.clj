805b73e352e38fb9a51c1656513f97adf5c83ad3
Dale Cameron <fortiscontego@gmail.com>
2013-10-31 12:42:33 -0500

    inital project creation

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject storm-elasticsearch "0.0.1-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
+  :javac-options ["-source" "1.6" "-target" "1.6"]
+  :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib"]
+  :aot :all
+  :dependencies [[org.elasticsearch/elasticsearch "0.90.5"]]
+  :profiles {:provided
+             {:dependencies [[storm "0.8.2"]]}}
+)
+
